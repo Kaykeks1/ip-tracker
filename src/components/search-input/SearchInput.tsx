e7eb137ac63f135047956d1a047256500bd01fca
kayode <kayuskeks@gmail.com>
2024-01-22 22:32:45 +0100

    added placeholder

--- src/components/search-input/SearchInput.tsx
+++ src/components/search-input/SearchInput.tsx
@@ -18,7 +18,7 @@ function SearchInput({ preValue, handleSubmit }: Prop) {
   }
   return (
     <form className='search-input' onSubmit={(e) => handleSubmit(e, value)}>
-      <input type="text" placeholder="8.8.8.8" value={value} onChange={handleInputChange} />
+      <input type="text" placeholder="_._._._" value={value} onChange={handleInputChange} />
       <button type="submit">
         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" fill="white" /></svg>
       </button>
